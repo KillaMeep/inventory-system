1b187aafe1024e1842b32d2124303126a81346da
KillaMeep <45155241+KillaMeep@users.noreply.github.com>
2023-09-07 19:15:43 -0600

    Update status.py

--- utils/status.py
+++ utils/status.py
@@ -2,6 +2,7 @@ from termcolor import colored,cprint
 import os
 os.system('color')
 
+#create a status class for better formatting in print statements
 class status():
     OK = colored('[OK]','green')
     WARN = colored('[WARN]','yellow')
